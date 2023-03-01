cfcc09a9d6668940bba3d92f1af386ce97416fcb
Svamei <357810508@qq.com>
2023-03-01 09:14:49 +0800

    测试BeanDefinition与BeanFactory

--- src/main/java/com/kxs/springframework/BeanDefinition.java
+++ src/main/java/com/kxs/springframework/BeanDefinition.java
@@ -10,11 +10,12 @@ public class BeanDefinition {
 
     private Object bean;
 
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
     public Object getBean() {
         return bean;
     }
 
-    public void setBean(Object bean) {
-        this.bean = bean;
-    }
 }
---
+++ src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -0,0 +1,15 @@
+package cn.bugstack.springframework.test.bean;
+
+/**
+ * @ClassName UserService
+ * @Description
+ * @Author Svamei
+ * @Date 9:06 2023/3/1
+ **/
+public class UserService {
+
+    public void queryUserInfo(){
+        System.out.println("查询用户信息");
+    }
+
+}
